d11313ccca222c096ebd3422f3fad0bb410f17df
hyochan <dooboolab@gmail.com>
2018-12-25 08:44:10 +0900

    Add e6 examples.

---
+++ es6/destructure_object_array.js
@@ -0,0 +1,32 @@
+const address = {
+  country: 'South Korea',
+  city: 'Seoul',
+  street: 'Gangnam',
+  str_num: 141,
+  postcode: '00510',
+};
+
+// 객체 비구조화, Object Destructuring.
+const country = 'Japan';
+const city = 'Osaka';
+
+const { country: nextCountry, city: nextCity } = address;
+console.log(`${nextCountry}, ${nextCity}`);
+
+// 배열 비구조화, Array Destructuring.
+const [thirdCountry] = ['Japan', 'South Korea', 'America'];
+console.log(thirdCountry);
+
+// 객체 리터럴, Object Literal.
+function getAddress(country, city, street) {
+  const myAddress = {
+    country,
+    city,
+    street,
+    str_num: 888,
+    postcode: '9999',
+  };
+  console.log(myAddress);
+}
+
+getAddress('Japan', 'Osaka', 'street');
